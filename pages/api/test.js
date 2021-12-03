5d56d5ae9612910fcfd3c438be064956a818047d
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-12-03 11:33:13 +0600

    Adding Layout & Navigation Bar

---
+++ pages/api/test.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// export default function helloAPI(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+
+export default (req, res) => {
+  res.json({ message: 'HEllo World' })
+}
